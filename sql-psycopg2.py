e47efbdc83d6553281175622a2824b43b6100c67
juliandunne1234 <julianmorandunne@gmail.com>
2022-08-01 13:19:57 +0000

    psycopg2 and sqlalchemy tutorial

---
+++ sql-psycopg2.py
@@ -0,0 +1,42 @@
+import psycopg2
+
+# connect to "chinook" database
+connection = psycopg2.connect(database="chinook")
+
+# build a cursor object of the database
+cursor = connection.cursor()
+
+# Query 1 - select all records from the "Artist" table
+# Import for psycopg2 format that single quotes contain the statement
+# cursor.execute('SELECT * FROM "Artist"')
+
+# Query 2 - select only the "Name" column from "Artist" table
+# cursor.execute('SELECT "Name" FROM "Artist"')
+
+# Query 3 - select only "Queen" from "Artist" table
+# cursor.execute('SELECT * FROM "Artist" WHERE "Name" = %s', ["Queen"])
+
+# Query 4 - select only by "ArtistId" #51 from the "Artist" table
+# cursor.execute('SELECT * FROM "Artist" WHERE "ArtistId" = %s', [51])
+
+# Query 5 - select only the albums with "ArtistId" #51 on the "Album" table
+# cursor.execute('SELECT * FROM "Album" WHERE "ArtistId" = %s', [51])
+
+# Query 6 - select all tracks where the composer is "Queen" from the "Track" table
+# cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["Queen"])
+
+# Query 7 - select all tracks where the composer is "Test" from the "Track" table
+cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["Test"])
+
+# fetch the results (multiple)
+results = cursor.fetchall()
+
+# fetch the results (single)
+# results = cursor.fetchone()
+
+# close the connection
+connection.close()
+
+# print results
+for result in results:
+    print(result)
